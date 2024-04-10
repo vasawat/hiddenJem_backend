b78a4971ca65049dbd785cdc16cd29c8d39b1b30
Mr.Boss <vasawat77@gmail.com>
2024-04-10 17:41:30 +0700

    fix path

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://hiddenjemvasawat.netlify.app",
     credentials: true,
   })
 );
